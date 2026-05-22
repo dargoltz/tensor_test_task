68a6c4d41920ec451bb6a82e81949113dd299b55
andrey.lesnov <andreythecoolguy@gmail.com>
2026-05-22 18:36:02 +0300

    feat: implement cli methods, add dotenv uasges

--- tensor_test_task/cli.py
+++ tensor_test_task/cli.py
@@ -1,4 +1,9 @@
+import os
+import json
+
 import typer
+from psycopg.errors import UniqueViolation, DatabaseError
+from .service import import_data_from_json_file, get_organization_employees
 
 app = typer.Typer()
 
@@ -8,7 +13,29 @@ def import_data() -> None:
     """
     Добавляет данные об организациях и сотрудниках из JSON файла в БД.
     """
-    ...
+    try:
+        path = os.getenv("DEFAULT_DATA_PATH")
+
+        if not path:
+            typer.echo("Не указан путь к файлу с данными для импорта")
+            return
+
+        import_data_from_json_file(path)
+
+        typer.echo("Импорт завершен")
+    except (KeyError, TypeError, json.JSONDecodeError):
+        typer.echo("JSON файл составлен некорректно")
+    except FileNotFoundError:
+        typer.echo("Файл для импорта отсутствует или адрес указан некорректно")
+    except PermissionError:
+        typer.echo("Нет прав на чтение файла или проверку размера")
+    except UniqueViolation:
+        typer.echo("Данные из JSON файла уже импортированы")
+    except DatabaseError:
+        typer.echo("Ошибка при выполнении запроса")
+    except ValueError:
+        typer.echo("Файл слишком большой. Максимальный размер - 10Мб")
+
 
 @app.command()
 def get_employees(employee_id: int) -> None:
@@ -18,4 +45,13 @@ def get_employees(employee_id: int) -> None:
     Args:
         employee_id (int): id сотрудника
     """
-    ...
+    try:
+        employees = get_organization_employees(employee_id)
+
+        typer.echo("Cписок сотрудников:")
+        for employee in employees:
+            typer.echo(employee)
+    except ValueError:
+        typer.echo("Необходимо указать ID сотрудника")
+    except DatabaseError:
+        typer.echo("Ошибка при выполнении запроса")
--- tensor_test_task/main.py
+++ tensor_test_task/main.py
@@ -1,5 +1,7 @@
 import typer
 
+from dotenv import load_dotenv
+
 from .cli import app as cli_app_commands
 
 app = typer.Typer()
@@ -7,4 +9,5 @@ app = typer.Typer()
 app.add_typer(cli_app_commands)
 
 if __name__ == "__main__":
+    load_dotenv()
     app()
