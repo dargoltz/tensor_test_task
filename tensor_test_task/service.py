8ad3b3a5fd44927235318012b84f60f31a065708
andrey.lesnov <andreythecoolguy@gmail.com>
2026-05-22 17:45:15 +0300

    feat: implement import data method

--- tensor_test_task/service.py
+++ tensor_test_task/service.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+from tensor_test_task.db import batch_insert_hierarchy_items
 
 
 def import_data_from_json_file(path: str, max_size: int = 10 * 1024 * 1024):
@@ -11,10 +12,15 @@ def import_data_from_json_file(path: str, max_size: int = 10 * 1024 * 1024):
         path (str): путь к файлу
         max_size (int): максимальный размер файла в байтах, по умолчанию 10Мб
     """
-    # open file
-    # parse json
-    # batch insert
-    ...
+    if os.path.getsize(path) > max_size:
+        raise ValueError
+
+    with open(path, "r") as f:
+        data = json.load(f)
+
+    hierarchy_item_values = parse_json(data)
+
+    batch_insert_hierarchy_items(hierarchy_item_values)
 
 
 def parse_json(data) -> list[tuple[int, int | None, str, str]]:
@@ -24,7 +30,17 @@ def parse_json(data) -> list[tuple[int, int | None, str, str]]:
     Args:
         data: list[dict[str, Any]]
     """
-    ...
+    result = []
+
+    for item in data:
+        pk = item["id"]
+        parent_id = item["ParentId"]
+        name = item["Name"]
+        hierarchy_item_type = str(item["Type"])  # str тип тк в БД у нас лежит enum
+
+        result.append((pk, parent_id, name, hierarchy_item_type))
+
+    return result
 
 def get_organization_employees(employee_id: int) -> list[str]:
     """
